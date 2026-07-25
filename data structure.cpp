d85ce4a58878a935c7d1ad23d225bdd2ce630478
Mahmoud Selmi <your_github_email@example.com>
2026-07-25 20:46:37 +0300

    implement union structure and update main function to demonstrate usage

--- data structure.cpp
+++ data structure.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
+//2#union
+union box{
 
-int main() {
-  cout << "H";
-  return 0;
+double weight;
+double heigt;
+
+
+};
+int main()
+{
+
+box b1;
+b1.weight=20;
+b1.heigt=30;
+cout<<b1.heigt<< endl;
+cout<< b1.heigt;
 }
+//#3 linked list

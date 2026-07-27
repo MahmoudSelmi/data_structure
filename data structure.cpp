87e3c4bae8a5b093a56ab0c03fd7810bfe9d03b8
Mahmoud Selmi <your_github_email@example.com>
2026-07-27 11:43:06 +0300

    implement stack and linked list structure with basic operations

--- data structure.cpp
+++ data structure.cpp
@@ -18,3 +18,47 @@ cout<<b1.heigt<< endl;
 cout<< b1.heigt;
 }
 //#3 linked list
+void insertNode(int value){
+  
+  
+  }  
+
+box b1;
+b1.weight=20;
+b1.heigt=30;
+cout<<b1.heigt<< endl;
+cout<< b1.heigt;
+//stack implementation
+#define size 5
+int stack[size],top=-1;
+void puch(int value);
+int pop();
+int main(){
+
+
+}
+void puch(int value){
+if (top==size-1){
+    cout<< "stack overflow"<<endl;
+
+}
+else{
+
+    top++;
+    stack[top]=value;
+
+}
+
+}
+int pop(){
+    if (top== -1){
+
+        cout << "stack underflow";
+
+    }
+    else{
+        return stack[top--];
+    }
+}
+//queue 
+
